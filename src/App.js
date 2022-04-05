b83aeebb3cd8d9dd52b0014bd9bd7f664bd0e544
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-04-05 21:51:11 +0600

    active link added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-// import { Route, Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Blogs from './components/Blogs/Blogs';
 import Dashboard from './components/Dashboard/Dashboard';
-// import Home from './components/Home/Home'
-// import Reviews from './components/Reviews/Reviews'
-// import Dashboard from './components/Dashboard/Dashboard'
-// import Blogs from './components/Blogs/Blogs'
-// import About from './components/About/About'
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NoMatch from './components/NoMatch/NoMatch';
@@ -38,14 +32,6 @@ function App() {
         <Route path='/about' element={<About></About>} />
         <Route path='*' element={<NoMatch></NoMatch>} />
       </Routes>
-      {/* <Routes>
-        <Route path="/" element={<Home></Home>} />
-        <Route path="/home" element={<Home></Home>} />
-        <Route path="/reviews" element={<Reviews></Reviews>} />
-        <Route path="/dashboard" element={<Dashboard></Dashboard>} />
-        <Route path="/blogs" element={<Blogs></Blogs>} />
-        <Route path="/about" element={<About></About>} />
-      </Routes> */}
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,59 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className='py-5 mb-20'>
-            <Link className='text-3xl mx-10 hover:text-sky-600' to="/home">Home</Link>
-            <Link className='text-3xl mx-10 hover:text-sky-600' to="/reviews">Reviews</Link>
-            <Link className='text-3xl mx-10 hover:text-sky-600' to="/dashboard">Dashboard</Link>
-            <Link className='text-3xl mx-10 hover:text-sky-600' to="/blogs">Blogs</Link>
-            <Link className='text-3xl mx-10 hover:text-sky-600' to="/about">About</Link>
+            <NavLink
+                style={({ isActive }) => {
+                    return {
+                        textDecoration: isActive ? "underline" : "",
+                        color: isActive ? "blue" : "",
+                    };
+                }}
+                className='text-3xl mx-10 hover:text-sky-600'
+                to="/home"
+            >Home</NavLink>
+            <NavLink
+                style={({ isActive }) => {
+                    return {
+                        textDecoration: isActive ? "underline" : "",
+                        color: isActive ? "blue" : "",
+                    };
+                }}
+                className='text-3xl mx-10 hover:text-sky-600'
+                to="/reviews"
+            >Reviews</NavLink>
+            <NavLink
+                style={({ isActive }) => {
+                    return {
+                        textDecoration: isActive ? "underline" : "",
+                        color: isActive ? "blue" : "",
+                    };
+                }}
+                className='text-3xl mx-10 hover:text-sky-600'
+                to="/dashboard"
+            >Dashboard</NavLink>
+            <NavLink
+                style={({ isActive }) => {
+                    return {
+                        textDecoration: isActive ? "underline" : "",
+                        color: isActive ? "blue" : "",
+                    };
+                }}
+                className='text-3xl mx-10 hover:text-sky-600'
+                to="/blogs"
+            >Blogs</NavLink>
+            <NavLink
+                style={({ isActive }) => {
+                    return {
+                        textDecoration: isActive ? "underline" : "",
+                        color: isActive ? "blue" : "",
+                    };
+                }}
+                className='text-3xl mx-10 hover:text-sky-600'
+                to="/about"
+            >About</NavLink>
         </nav>
     );
 };
